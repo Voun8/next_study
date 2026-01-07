02fd7f7bdd3f128885313ed33a63a6468e798a1f
lyy <liyiyang0731@gmail.com>
2026-01-07 14:21:47 +0800

    23_平行路由

---
+++ src/app/@team/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='flex justify-center items-center text-white h-60 rounded-lg bg-teal-500 flex-1'>team</div>
+  )
+}
--- src/app/default.tsx
+++ src/app/default.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Page() {
   return (
     <div className='flex mt-6 p-6 rounded-lg bg-gray-500 text-white flex-1 h-32'>
-      @children
+      @children default
     </div>
   )
 }
