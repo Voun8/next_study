93ea9a1ee80130f54512862c09b53fef9a9ff8bd
lyy <liyiyang0731@gmail.com>
2026-01-13 15:02:35 +0800

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,18 @@
-'use client'
-import React, { useEffect } from 'react'
-export default function Page() {
-  const [todos, setTodos] = React.useState([])
 
-  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+import React from 'react'
+import { addTodo, getTodos } from './actions'
 
-    const res=await fetch('http://localhost:3000/api/todos', {
-      method: 'POST',
-      body: new FormData(e.currentTarget)
-    })
-
-    const {data} = await res.json()
-    setTodos(data)
-    
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await (await fetch('http://localhost:3000/api/todos')).json()
-      setTodos(data)
-    }
-    fetchData()
-  }, [])
-
+export default async function Page() {
+  const todos=await getTodos()
   return (
     <div className="p-10">
-      <form onSubmit={handleSubmit}>
+      <form action={addTodo}>
         <input type="text" name="todo" className='border p-2' />
         <button type="submit" className='border p-2 ml-2'>Add</button>
       </form>
       <ul className='leading-8 mt-4'>
         {
-          todos.map((todo,index) => (
+          todos.map((todo,index)=>(
             <li key={index}>{todo}</li>
           ))
         }
